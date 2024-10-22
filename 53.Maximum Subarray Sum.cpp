b38c1981b94fed2f97646b4510f821f9bcc7b3ba
Noor-ul-ain <shknoor613@gmail.com>
2024-10-22 20:12:51 +0500

    Add files via upload

---
+++ 53.Maximum Subarray Sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   
+    int maxSubArray(vector<int>& nums) {
+    int currsum = 0 , maxSum = INT_MIN;
+    for (int val : nums)
+    {
+        currsum += val ; 
+        maxSum = max(currsum  , maxSum  );
+        if (currsum < 0){
+            currsum = 0 ;
+        }
+
+    }  
+    return maxSum ;  
+    }
+
+}
